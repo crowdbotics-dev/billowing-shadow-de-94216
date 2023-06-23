fd2338289aadf97c28e69316494a4e277eb97417
Crowdbotics <engineering@crowdbotics.com>
2023-06-23 23:13:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "billowing-shadow-de-94216",
+  url: "https://billowing-shadow-de-94216.botics.co",
+  api: "https://billowing-shadow-de-94216.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
